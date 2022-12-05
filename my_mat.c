b21df42ed61a140b2c49db9de102869bc72528c3
Dor <dor3518@gmail.com>
2022-12-05 15:52:29 +0200

    St

--- my_mat.c
+++ my_mat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-void A(int Mat[size][size])
+void A(int Mat[size][size]) \\ Option A
 {
     int n;
     for (int i = 0; i < size; i++)
@@ -14,4 +14,26 @@ void A(int Mat[size][size])
     }
 }
 
-void B()
+void B(int Mat[size][size], int i, int j) \\ Option B
+{
+    if (Mat[i][j] == 0)
+    {
+        printf("False\n");
+    }
+    else
+    {
+        printf("True\n");
+    }
+}
+
+void C(int Mat[size][size], int i, int j) \\ Option C
+{
+    if (Mat[i][j] == 0)
+    {
+        printf("-1\n");
+    }
+    else
+    {
+        printf("%d", Mat[i][j]);
+    }
+}
