6ebb1fda091369cc3ba532f93a6d1774bad0b455
Dor <dor3518@gmail.com>
2022-12-06 16:39:45 +0200

    AF

--- my_mat.c
+++ my_mat.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include "my_mat.h"
 
+void DistMat(Mat[size][size])
+{
+    for (int k = 0; k < size; k++)
+    {
+        for (int i = 0; k < size; i++)
+        {
+            for (int j = 0; k < size; j++)
+            {
+                if (i != j && Mat[i][k] != 0 && Mat[k][j] != 0)
+                {
+                    Mat[i][j] = Mat[i][k] + Mat[k][j];
+                }
+                Mat[i][j] = min(Mat[i][j], Mat[i][k] + Mat[k][j])
+            }
+        }
+    }
+}
+int min(int a, int b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+    if (a == b)
+    {
+        return a;
+    }
+}
 void A(int Mat[size][size]) \\ Option A
 {
     int n;
@@ -12,6 +44,7 @@ void A(int Mat[size][size]) \\ Option A
             Mat[i][j] = n;
         }
     }
+    DistMat(Mat);
 }
 
 void B(int Mat[size][size], int i, int j) \\ Option B
