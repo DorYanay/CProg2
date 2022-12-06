df02d757945665ce55977ac8998268939934b6b2
Dor <dor3518@gmail.com>
2022-12-06 19:36:37 +0200

    F

--- my_mat.h
+++ my_mat.h
@@ -1,5 +1,6 @@
 #define SIZE 10
 void A(int Mat[SIZE][SIZE]);
-int B(int Mat[SIZE][SIZE], int, int);
-int C(int Mat[SIZE][SIZE], int, int);
+void B(int Mat[SIZE][SIZE], int, int);
+void C(int Mat[SIZE][SIZE], int, int);
+void DistMat(int Mat[SIZE][SIZE]);
 int min(int, int);
